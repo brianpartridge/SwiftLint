4b3c90ce31c1897d530cbf19522dd0d0cad92acb
Brian Partridge <brianpartridge@squareup.com>
2015-12-21 01:27:09 -0500

    Working!

--- Source/SwiftLintFramework/Rules/ColonRule.swift
+++ Source/SwiftLintFramework/Rules/ColonRule.swift
@@ -58,60 +58,60 @@ public struct ColonRule: CorrectableRule {
             "func abc(def:  Void) {}\n": "func abc(def: Void) {}\n",
             "func abc(def :Void) {}\n": "func abc(def: Void) {}\n",
             "func abc(def : Void) {}\n": "func abc(def: Void) {}\n",
-            "func abc(def: Void, ghi :Void) {}\n": "func abc(def: Void, ghi: Void) {}\n", //end
-//            "let abc: Void\n": "let abc: Void\n",
-//            "let abc: [Void: Void]\n": "let abc: [Void: Void]\n",
-//            "let abc: (Void, Void)\n": "let abc: (Void, Void)\n",
-//            "let abc: String=\"def\"\n": "let abc: String=\"def\"\n",
-//            "let abc: Int=0\n": "let abc: Int=0\n",
-//            "let abc: Enum=Enum.Value\n": "let abc: Enum=Enum.Value\n",
-//            "func abc(def: Void) {}\n": "func abc(def: Void) {}\n",
-//            "func abc(def: Void, ghi: Void) {}\n": "func abc(def: Void, ghi: Void) {}\n",
-            "// 周斌佳年周斌佳\nlet abc: String = \"abc:\"": "// 周斌佳年周斌佳\nlet abc: String = \"abc:\""
+            "func abc(def: Void, ghi :Void) {}\n": "func abc(def: Void, ghi: Void) {}\n"
         ]
     )
 
-    private let spacingLeftOfColonPattern = "(\\w+)\\s+:\\s*(\\S+)"
-    private let spacingRightOfColonPattern = "(\\w+):(?:\\s{0}|\\s{2,})(\\S+)"
+    // Use \S+? for lazy evaluation, otherwise the expansion could include undesired SyntaxKinds.
+    // Include \[? to handle the case where a type identifier is within '[]'.
+    private let spacingLeftOfColonPattern = "(\\w+)\\s+:\\s*(\\[?\\S+?)"
+    private let spacingRightOfColonPattern = "(\\w+):(?:\\s{0}|\\s{2,})(\\S+?)"
     private func patterns() -> [String] {
         return [spacingLeftOfColonPattern, spacingRightOfColonPattern]
     }
 
+    private func validMatchesInFile(file: File, withPattern pattern: String) -> [NSRange] {
+        return file.matchPattern(pattern).filter { range, syntaxKinds in
+            print(file.contents)
+            print(pattern)
+            if !syntaxKinds.startsWith([.Identifier, .Typeidentifier]) {
+                return false
+            }
+            if Set(syntaxKinds).intersect(Set(SyntaxKind.commentAndStringKinds())).count > 0 {
+                return false
+            }
+            return true
+        }.flatMap { $0.0 }
+    }
+
     public func validateFile(file: File) -> [StyleViolation] {
         let pattern = (patterns() as NSArray).componentsJoinedByString("|")
-
-        return file.matchPattern(pattern).flatMap { range, syntaxKinds in
-            if !syntaxKinds.startsWith([.Identifier, .Typeidentifier]) {
-                return nil
-            }
-
+        return validMatchesInFile(file, withPattern: pattern).flatMap { range in
             return StyleViolation(ruleDescription: self.dynamicType.description,
                 location: Location(file: file, offset: range.location))
         }
     }
 
     public func correctFile(file: File) -> [Correction] {
-        let replacementTemplate = "$1: $2"
+        return patterns().reduce([Correction]()) { corrections, pattern in
+            return corrections + correctFile(file, withPattern: pattern)
+        }
+    }
+
+    public func correctFile(file: File, withPattern pattern: String) -> [Correction] {
+        let matches = validMatchesInFile(file, withPattern: pattern)
+        guard !matches.isEmpty else { return [] }
+        let regularExpression = regex(pattern)
         var corrections = [Correction]()
         var contents = file.contents
-        let regexes = patterns().map { regex($0) }
-        for regex in regexes {
-            let range = NSRange(location: 0, length: contents.characters.count)
-            let matchingRanges = file.matchPattern(pattern, excludingSyntaxKinds: [SyntaxKind.commentAndStringKinds()])
-            for matchingRange in matchingRanges {
-                let description = self.dynamicType.description
-                let location = Location(file: file, offset: matchingRange.location)
-                corrections.append(Correction(ruleDescription: description, location: location))
-            }
-            contents = regex.stringByReplacingMatchesInString(contents,
-                options: [],
-                range: range,
-                withTemplate: replacementTemplate)
+        for range in matches.reverse() {
+            contents = regularExpression.stringByReplacingMatchesInString(contents,
+                options: [], range: range, withTemplate: "$1: $2")
+            let description = self.dynamicType.description
+            let location = Location(file: file, offset: range.location)
+            corrections.append(Correction(ruleDescription: description, location: location))
         }
-        if !corrections.isEmpty {
-            file.write(contents)
-            return corrections
-        }
-        return []
+        file.write(contents)
+        return corrections
     }
 }
