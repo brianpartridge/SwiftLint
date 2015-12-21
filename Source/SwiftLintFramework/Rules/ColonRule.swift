d2a19c4289d38f38a9ad25a342365c5d4aafc9e7
Brian Partridge <brianpartridge@squareup.com>
2015-12-20 23:15:20 -0500

    WIP: Mostly working, except for comments.

--- Source/SwiftLintFramework/Rules/ColonRule.swift
+++ Source/SwiftLintFramework/Rules/ColonRule.swift
@@ -8,7 +8,7 @@
 
 import SourceKittenFramework
 
-public struct ColonRule: Rule {
+public struct ColonRule: CorrectableRule {
     public static let description = RuleDescription(
         identifier: "colon",
         name: "Colon",
@@ -41,11 +41,44 @@ public struct ColonRule: Rule {
             "func abc(def :Void) {}\n",
             "func abc(def : Void) {}\n",
             "func abc(def: Void, ghi :Void) {}\n"
+        ],
+        corrections: [
+            "let abc:Void\n": "let abc: Void\n",
+            "let abc:  Void\n": "let abc: Void\n",
+            "let abc :Void\n": "let abc: Void\n",
+            "let abc : Void\n": "let abc: Void\n",
+            "let abc : [Void: Void]\n": "let abc: [Void: Void]\n",
+            "let abc :String=\"def\"\n": "let abc: String=\"def\"\n",
+            "let abc :Int=0\n": "let abc: Int=0\n",
+            "let abc :Int = 0\n": "let abc: Int = 0\n",
+            "let abc:Int=0\n": "let abc: Int=0\n",
+            "let abc:Int = 0\n": "let abc: Int = 0\n",
+            "let abc:Enum=Enum.Value\n": "let abc: Enum=Enum.Value\n",
+            "func abc(def:Void) {}\n": "func abc(def: Void) {}\n",
+            "func abc(def:  Void) {}\n": "func abc(def: Void) {}\n",
+            "func abc(def :Void) {}\n": "func abc(def: Void) {}\n",
+            "func abc(def : Void) {}\n": "func abc(def: Void) {}\n",
+            "func abc(def: Void, ghi :Void) {}\n": "func abc(def: Void, ghi: Void) {}\n", //end
+//            "let abc: Void\n": "let abc: Void\n",
+//            "let abc: [Void: Void]\n": "let abc: [Void: Void]\n",
+//            "let abc: (Void, Void)\n": "let abc: (Void, Void)\n",
+//            "let abc: String=\"def\"\n": "let abc: String=\"def\"\n",
+//            "let abc: Int=0\n": "let abc: Int=0\n",
+//            "let abc: Enum=Enum.Value\n": "let abc: Enum=Enum.Value\n",
+//            "func abc(def: Void) {}\n": "func abc(def: Void) {}\n",
+//            "func abc(def: Void, ghi: Void) {}\n": "func abc(def: Void, ghi: Void) {}\n",
+            "// 周斌佳年周斌佳\nlet abc: String = \"abc:\"": "// 周斌佳年周斌佳\nlet abc: String = \"abc:\""
         ]
     )
 
+    private let spacingLeftOfColonPattern = "(\\w+)\\s+:\\s*(\\S+)"
+    private let spacingRightOfColonPattern = "(\\w+):(?:\\s{0}|\\s{2,})(\\S+)"
+    private func patterns() -> [String] {
+        return [spacingLeftOfColonPattern, spacingRightOfColonPattern]
+    }
+
     public func validateFile(file: File) -> [StyleViolation] {
-        let pattern = "\\w+\\s+:\\s*\\S+|\\w+:(?:\\s{0}|\\s{2,})\\S+"
+        let pattern = (patterns() as NSArray).componentsJoinedByString("|")
 
         return file.matchPattern(pattern).flatMap { range, syntaxKinds in
             if !syntaxKinds.startsWith([.Identifier, .Typeidentifier]) {
@@ -55,5 +88,30 @@ public struct ColonRule: Rule {
             return StyleViolation(ruleDescription: self.dynamicType.description,
                 location: Location(file: file, offset: range.location))
         }
+    }
+
+    public func correctFile(file: File) -> [Correction] {
+        let replacementTemplate = "$1: $2"
+        var corrections = [Correction]()
+        var contents = file.contents
+        let regexes = patterns().map { regex($0) }
+        for regex in regexes {
+            let range = NSRange(location: 0, length: contents.characters.count)
+            let matchingRanges = file.matchPattern(pattern, excludingSyntaxKinds: [SyntaxKind.commentAndStringKinds()])
+            for matchingRange in matchingRanges {
+                let description = self.dynamicType.description
+                let location = Location(file: file, offset: matchingRange.location)
+                corrections.append(Correction(ruleDescription: description, location: location))
+            }
+            contents = regex.stringByReplacingMatchesInString(contents,
+                options: [],
+                range: range,
+                withTemplate: replacementTemplate)
+        }
+        if !corrections.isEmpty {
+            file.write(contents)
+            return corrections
+        }
+        return []
     }
 }
